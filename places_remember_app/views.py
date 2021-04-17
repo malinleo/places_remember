eb1ee986e93925b58516a9c1a05d2efa54c62b57
malinleo <mr.malin@list.ru>
2021-04-17 22:56:01 +0700

    Create "Your impressions" page

--- places_remember_app/views.py
+++ places_remember_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from places_remember_app.models import Memory
+from social_django.models import UserSocialAuth
 # Create your views here.
 
 
@@ -10,9 +11,11 @@ def main(request):
 
 @login_required(login_url='/user/social-auth/login/facebook/')
 def memories(request):
-    memories = Memory.objects.filter(user=request.user)
+    memories = Memory.objects.filter(user=request.user.pk)
+    user_social_auth = UserSocialAuth.objects.get(user_id=request.user.pk)
     context = {
         'user': request.user,
-        'memories': memories
+        'memories': memories,
+        'picture_url': user_social_auth.extra_data['picture']['data']['url'],
     }
     return render(request, 'impressions/memories.html', context)
